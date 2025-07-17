feceaee2b3626f725aed3cb3483d213adb3181a9
L. Arnold <larnold@nomadic.net>
2025-07-17 09:45:43 -0600

    Update to debug recaptcha_forms.js

--- static/src/js/recaptcha_forms.js
+++ static/src/js/recaptcha_forms.js
@@ -7,6 +7,7 @@ publicWidget.registry.RecaptchaForm = publicWidget.Widget.extend({
     selector: 'form:has(.g-recaptcha)',
     
     start: function () {
+        console.log('RecaptchaForm widget starting for:', this.$el[0]);
         this._super.apply(this, arguments);
         this._loadRecaptcha();
     },
@@ -14,37 +15,29 @@ publicWidget.registry.RecaptchaForm = publicWidget.Widget.extend({
     _loadRecaptcha: function () {
         var self = this;
         
-        // Check if reCAPTCHA is already loaded and ready
+        // Debug: Log current state
+        console.log('Loading reCAPTCHA for form:', this.$el[0]);
+        console.log('Found .g-recaptcha elements:', this.$('.g-recaptcha').length);
+        
         if (typeof grecaptcha !== 'undefined' && grecaptcha.render) {
             this._initRecaptcha();
-            return;
-        }
-        
-        // If reCAPTCHA script is not loaded, load it
-        if (!window.recaptchaScriptLoaded) {
-            window.recaptchaScriptLoaded = true;
-            
-            // Create a global callback function that will be called when reCAPTCHA is ready
-            window.onRecaptchaReady = function() {
-                // Find all RecaptchaForm widgets and initialize them
-                var widgets = publicWidget.registry.RecaptchaForm.prototype._getWidgets();
-                widgets.forEach(function(widget) {
-                    if (widget._initRecaptcha) {
-                        widget._initRecaptcha();
-                    }
-                });
-            };
+        } else if (!window.recaptchaLoading) {
+            window.recaptchaLoading = true;
             
             var script = document.createElement('script');
-            script.src = 'https://www.google.com/recaptcha/api.js?onload=onRecaptchaReady&render=explicit';
-            script.async = true;
-            script.defer = true;
+            script.src = 'https://www.google.com/recaptcha/api.js';
+            script.onload = function() {
+                setTimeout(function() {
+                    self._initRecaptcha();
+                }, 100);
+            };
             script.onerror = function() {
                 console.error('Failed to load reCAPTCHA script');
+                window.recaptchaLoading = false;
             };
             document.head.appendChild(script);
         } else {
-            // Script is loading, wait for the callback
+            // Script is already loading, wait for it
             var checkReady = function() {
                 if (typeof grecaptcha !== 'undefined' && grecaptcha.render) {
                     self._initRecaptcha();
@@ -52,62 +45,53 @@ publicWidget.registry.RecaptchaForm = publicWidget.Widget.extend({
                     setTimeout(checkReady, 100);
                 }
             };
-            checkReady();
+            setTimeout(checkReady, 100);
         }
     },
     
     _initRecaptcha: function () {
         var self = this;
+        
+        // Only initialize reCAPTCHA elements within THIS widget's form
         var recaptchaElements = this.$('.g-recaptcha');
         
+        console.log('Initializing reCAPTCHA for', recaptchaElements.length, 'elements in form:', this.$el[0]);
+        
         recaptchaElements.each(function(index, element) {
+            console.log('Processing element:', element, 'widgetId:', element.dataset.widgetId);
+            
             // Check if this element has already been initialized
             if (!element.dataset.widgetId && element.dataset.sitekey) {
                 try {
-                    var widgetId = grecaptcha.render(element, {
-                        'sitekey': element.dataset.sitekey,
-                        'callback': function(response) {
-                            // Optional: Handle successful reCAPTCHA completion
-                            console.log('reCAPTCHA completed');
-                        },
-                        'expired-callback': function() {
-                            // Optional: Handle reCAPTCHA expiration
-                            console.log('reCAPTCHA expired');
-                        }
-                    });
+                    console.log('Rendering reCAPTCHA for element:', element);
+                    var widgetId = grecaptcha.render(element);
                     element.dataset.widgetId = widgetId;
+                    console.log('Successfully rendered reCAPTCHA with widgetId:', widgetId);
                 } catch (error) {
                     console.error('Error rendering reCAPTCHA:', error);
                 }
+            } else {
+                console.log('Element already initialized or missing sitekey:', element);
             }
         });
     },
     
-    // Helper method to get all widget instances
-    _getWidgets: function() {
-        return publicWidget.registry.RecaptchaForm.prototype._instances || [];
+    destroy: function() {
+        // Clean up reCAPTCHA widgets when destroying
+        var recaptchaElements = this.$('.g-recaptcha');
+        recaptchaElements.each(function(index, element) {
+            if (element.dataset.widgetId && typeof grecaptcha !== 'undefined' && grecaptcha.reset) {
+                try {
+                    grecaptcha.reset(element.dataset.widgetId);
+                    delete element.dataset.widgetId;
+                } catch (error) {
+                    console.error('Error resetting reCAPTCHA:', error);
+                }
+            }
+        });
+        
+        this._super.apply(this, arguments);
     }
 });
-
-// Store widget instances for global callback
-publicWidget.registry.RecaptchaForm.prototype._instances = [];
-
-// Override the start method to track instances
-var originalStart = publicWidget.registry.RecaptchaForm.prototype.start;
-publicWidget.registry.RecaptchaForm.prototype.start = function() {
-    publicWidget.registry.RecaptchaForm.prototype._instances.push(this);
-    return originalStart.apply(this, arguments);
-};
-
-// Override the destroy method to clean up instances
-var originalDestroy = publicWidget.registry.RecaptchaForm.prototype.destroy;
-publicWidget.registry.RecaptchaForm.prototype.destroy = function() {
-    var instances = publicWidget.registry.RecaptchaForm.prototype._instances;
-    var index = instances.indexOf(this);
-    if (index > -1) {
-        instances.splice(index, 1);
-    }
-    return originalDestroy.apply(this, arguments);
-};
 
 });
