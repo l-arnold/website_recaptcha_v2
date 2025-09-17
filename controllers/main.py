d8bdd83db26cf1110c52b551bcad04a0feb33c8f
L. Arnold <larnold@nomadic.net>
2025-09-17 00:00:15 -0600

    Major rewrite of Main.py
    Login was not enforcing Recaptcha.  Assume same also on signup and other classes

--- controllers/main.py
+++ controllers/main.py
@@ -2,6 +2,7 @@ from odoo import http
 from odoo.http import request
 from odoo.addons.portal.controllers.portal import CustomerPortal
 from odoo.addons.auth_signup.controllers.main import AuthSignupHome
+from odoo.addons.web.controllers.main import Home
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 class RecaptchaCustomerPortal(CustomerPortal):
@@ -14,8 +15,6 @@ class RecaptchaCustomerPortal(CustomerPortal):
         if not recaptcha_response:
             return False
             
-        # Add your recaptcha validation logic here
-        # This should use the existing validation method from website_recaptcha_v2
         return request.website._validate_recaptcha(recaptcha_response)
 
 class RecaptchaAuthSignup(AuthSignupHome):
@@ -34,21 +33,30 @@ class RecaptchaAuthSignup(AuthSignupHome):
     def web_auth_signup(self, *args, **kw):
         if request.httprequest.method == 'POST':
             if not self._validate_recaptcha():
-                # Actually block the signup and show error
                 qcontext = self.get_auth_signup_qcontext()
                 qcontext['error'] = "reCAPTCHA verification failed. Please try again."
                 return request.render('auth_signup.signup', qcontext)
         return super(RecaptchaAuthSignup, self).web_auth_signup(*args, **kw)
 
-    @http.route('/web/login', type='http', auth='none', website=True)
-    def web_auth_login(self, redirect=None, **kw):
+class RecaptchaHome(Home):
+    
+    def _validate_recaptcha(self):
+        if not request.website.recaptcha_v2_enabled:
+            return True
+        
+        recaptcha_response = request.params.get('g-recaptcha-response')
+        if not recaptcha_response:
+            return False
+            
+        return request.website._validate_recaptcha(recaptcha_response)
+    
+    @http.route('/web/login', type='http', auth="none", website=True)
+    def web_login(self, redirect=None, **kw):
         if request.httprequest.method == 'POST':
             if not self._validate_recaptcha():
-                # Handle login reCAPTCHA failure
                 values = request.params.copy()
                 values['error'] = "reCAPTCHA verification failed. Please try again."
                 return request.render('web.login', values)
-        return super(RecaptchaAuthSignup, self).web_auth_login(redirect, **kw)
-
+        return super(RecaptchaHome, self).web_login(redirect, **kw)
 
 # Similar extensions for other controllers...
