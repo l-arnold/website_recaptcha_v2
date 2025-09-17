5cce35c98b4aef3740250173feafa3e48849adfb
L. Arnold <larnold@nomadic.net>
2025-09-16 23:38:42 -0600

    Update Main.py to handle failures and actually block the signup and show errors.

--- controllers/main.py
+++ controllers/main.py
@@ -20,12 +20,24 @@ class RecaptchaCustomerPortal(CustomerPortal):
 
 class RecaptchaAuthSignup(AuthSignupHome):
     
+    def _validate_recaptcha(self):
+        if not request.website.recaptcha_v2_enabled:
+            return True
+        
+        recaptcha_response = request.params.get('g-recaptcha-response')
+        if not recaptcha_response:
+            return False
+            
+        return request.website._validate_recaptcha(recaptcha_response)
+    
     @http.route('/web/signup', type='http', auth='public', website=True, sitemap=False)
     def web_auth_signup(self, *args, **kw):
         if request.httprequest.method == 'POST':
             if not self._validate_recaptcha():
-                # Handle recaptcha failure
-                pass
+                # Actually block the signup and show error
+                qcontext = self.get_auth_signup_qcontext()
+                qcontext['error'] = "reCAPTCHA verification failed. Please try again."
+                return request.render('auth_signup.signup', qcontext)
         return super(RecaptchaAuthSignup, self).web_auth_signup(*args, **kw)
 
 # Similar extensions for other controllers...
